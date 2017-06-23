1ecebfde22103a3a06e4a4ccf124e8efdde249cd
melchauu <melchauu@users.noreply.github.com>
2017-06-22 22:35:19 -0400

    Update tries.java

--- tries.java
+++ tries.java
@@ -6,14 +6,19 @@ import java.util.regex.*;
 public class TrieNode{
     protected boolean isHead = false;
     protected boolean isWordEnd = false;
+    protected int numChildren = 0;
     protected char letter ;
     protected TrieNode[] nextLetters= new TrieNode[26];
     
     public TrieNode setNextLetters (char ltr){
-        int asciiLetter = (int)ltr;
+        int asciiLetter = (int)ltr - 97;
         letter = ltr;
         nextLetters[asciiLetter]= new TrieNode();
         return nextLetters[asciiLetter];
+    }
+    public TrieNode setLastChar (char ltr){
+        isWordEnd = true;
+        return this.setNextLetters(ltr);
     }
     public TrieNode (){}
     public TrieNode (char ltr, boolean head){
@@ -26,32 +31,56 @@ public class TrieNode{
     
 }
 public class Solution {
-
+    protected TrieNode headNode = null;
+    protected void setHead (TrieNode node){
+        headNode = node;
+    }
+    public static int findContact(String prefix){
+        TrieNode curNode = headNode;
+        boolean matched = true;
+        for (int i=0; i < prefix.length() && matched; i++){
+           char ltr = prefix.charAt[i];
+           int asciiLetter = (int)ltr - 97;
+           if(curNode.nextLetters[asciiLetter] == null){
+               matched = false;
+           }
+           else{
+               curNode = curNode.nextLetters[asciiLetter];
+           }
+        }
+        if (matched == false ){
+            return 0;
+        }
+        return curNode.getNumChildren();
+    }
+    public static void addContact(String contact){
+        TrieNode curNode;
+        if (!(headNode==null)){
+            curNode=headNode;
+        }
+        else{
+            this.setHead(new TrieNode(contact.charAt[0], true));
+        }
+        for (int i=1; i< (contact.length() -1) ; i++){
+            curNode = curNode.setNextLetters(contact.charAt[i]);
+        }
+        // handle last char in contact
+        curNode.setLastChar.(contact.charAt[i]);
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        boolean headInitialized = false;
-        TrieNode headNode;
+
         TrieNode curNode;
         for(int a0 = 0; a0 < n; a0++){
             String op = in.next();
-            String contact = in.next();
+            String contactStr = in.next();
             
-            if (headInitialized && op.equals("add")){
-                
+            if (op.equals("add")){
+                this.addContact(contactStr);
             }
             else if (op.equals("find")){
-                
-            }
-            else if (!headInitialized && op.equals("add")){
-                //handle first char in contact
-                headNode = new TrieNode(contact.charAt[0], true);
-                curNode = headNode;
-                
-                for (int i=1; i< contact.length() ; i++){
-                    curNode = curNode.setNextLetters(contact.charAt[i]);
-                }
-                
+                this.findContact(contactStr);   
             }
         }
     }
